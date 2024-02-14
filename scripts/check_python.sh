bc83cc4853a25ca2c6b6bfe8ce3875c743ad1b1a
Adrian Tanase <158503737+Ari-Tanase@users.noreply.github.com>
2024-02-14 17:19:33 -0600

    add check python.sh

---
+++ scripts/check_python.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export REQUIRED_PYTHON_VERSION=$(cat pyproject.toml | grep python | awk '{print$3}' | tr -d \" | tr -d ^)
+echo ${REQUIRED_PYTHON_VERSION}
+
+export INSTALLED_PYTHON_VERSION=$(python --version --tag-only)
+echo $(INSTALLED_PYTHON_VERSION)
