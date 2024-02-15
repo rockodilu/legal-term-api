f9089bb9381a6c29c6b873f099515daee326237b
Adrian Tanase <158503737+Ari-Tanase@users.noreply.github.com>
2024-02-15 17:43:56 -0600

    fix

--- scripts/check_python.sh
+++ scripts/check_python.sh
@@ -2,9 +2,4 @@
 REQUIRED_PYTHON_VERSION=$(cat pyproject.toml | grep python | awk '{print$3}' | tr -d \" | tr -d ^)
 echo ${REQUIRED_PYTHON_VERSION}
 
-INSTALLED_PYTHON_VERSION=$(python --version | awk '{print$2}')
-echo ${INSTALLED_PYTHON_VERSION}
-
-if ${INSTALLED_PYTHON_VERSION} < ${REQUIRED_PYTHON_VERSION}; then
-    sudo apt-get install python ${REQUIRED_PYTHON_VERSION} -y
-fi
+export REQUIRED_PYTHON_VERSION=${REQUIRED_PYTHON_VERSION}
