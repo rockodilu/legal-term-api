40e67555891e197bae347232cb271fff6d7a15f7
Adrian Tanase <158503737+Ari-Tanase@users.noreply.github.com>
2024-02-15 17:51:58 -0600

    fix

--- scripts/check_python.sh
+++ scripts/check_python.sh
@@ -3,3 +3,5 @@ REQUIRED_PYTHON_VERSION=$(cat pyproject.toml | grep python | awk '{print$3}' | t
 echo ${REQUIRED_PYTHON_VERSION}
 
 export REQUIRED_PYTHON_VERSION=${REQUIRED_PYTHON_VERSION}
+
+echo ${REQUIRED_PYTHON_VERSION}
